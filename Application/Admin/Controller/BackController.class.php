3674c0ed46666956b02b1710659e314926ea5281
zhang520 <2269025237@qq.com>
2017-06-05 16:52:18 +0800

    backcontorller

--- Application/Admin/Controller/BackController.class.php
+++ Application/Admin/Controller/BackController.class.php
@@ -24,9 +24,13 @@ class BackController extends Controller {
 					$this->success('处理成功！', U('Index/index'));
 					exit();
 				}else{
+					//1:用户名不存在 2:密码错误
+					$this->sav($code=$loginStatus);
 					$loginStatus == 1 ? $this->error('用户名不存在！') : $this->error('密码错误！');;
 				}
 			}else{
+				//验证码错误记录 3代表验证码错误
+				$this->sav($code=3);
 				$this->error('处理失败！'. $adminModel->getError());
 			}
 		}
@@ -47,8 +51,8 @@ class BackController extends Controller {
 
     	// 实例化配置一定的参数
     	$config = array(    
-    		'imageW'  	  =>    180,    // 验证码宽度    
-    		'imageH'      =>    60,     // 验证码高度    
+    		'imageW'  	  =>    160,    // 验证码宽度
+    		'imageH'      =>    60,     // 验证码高度
     		'length'	  =>	3,		// 验证码长度
     		'useNoise'	  =>	false,	// 杂点
     		'useCurve'    =>	false,  // 混淆曲线
@@ -57,7 +61,7 @@ class BackController extends Controller {
     	$Verify = new \Think\Verify($config);
     	$Verify->entry();
     }
-	private function  sav(){
+	private function  sav($code=4){
 		$data['username']= I('post.username');
 		//当前时间
 		$data['currentTime']=time();
@@ -69,14 +73,28 @@ class BackController extends Controller {
 		$ip = get_client_ip();
 		//引入UTFWry.dat定位地点文件，tp框架原是没有的，需下载
 		$Ipdat = new IpLocation('UTFWry.dat');
-		$area = $Ipdat->getlocation($ip);;
-		$data['Location']=$area['country'];
-		var_dump($data);
-		exit();
+		$area = $Ipdat->getlocation($ip);
+		//定位地点为GBK，需转UT8存数据库
+		$data['Location']=mb_convert_encoding($area['country'],'utf-8', 'gbk');
+		if($code){
+			switch($code){
+				case 1:
+					$data['error']='用户名不存在';
+					break;
+				case 2:
+					$data['error']='密码错误';
+					break;
+				case 3:
+					$data['error']='验证码错误';
+					break;
+				case 4:
+					$data['error']='登录成功';
+					break;
+			}
+		}
 		//存入数据库表
 		$User = M("login");
 		$User->add($data);
 }
 }
-
  ?>
